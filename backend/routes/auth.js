f02257cfcbd3bac9913404d08c996731e6ae70d2
Kunal Kaushik <kunkaush1@publicisgroupe.net>
2022-03-31 18:07:44 +0530

    Creating ThunderClient Collections to Manage Requests.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,26 +1,38 @@
-const express = require('express')
-const User =require('../models/User')
-const router = express.Router()
-const { body, validationResult } = require('express-validator')
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-
-//Create a user using: POST "/api/auth/".Doesn't require auth
-router.post('/',[
-    body('name','Enter a valid name').isLength({min:3}),
-    body('email','Enter a valid email').isEmail(),
-    body('password').isLength({ min: 5 })
-],(req,res)=>{
+//Create a user using: POST "/api/auth/createuser".No login required
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    //If there is an error  return bad request and error
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    //Check weather the user with this email already present
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ error: "User with this email already present." });
+      }
+      user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-      }).then(user => res.json(user))
-      .catch(err=>{console.log(err)
-    res.json({error:'Please Enter a unique value for email.',message: err.message})})
-})
+      });
+      res.json(user);
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Some Error Occured.")
+    }
+  })
 
-module.exports=router
+module.exports = router;
