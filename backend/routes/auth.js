ac7332975857101fb77708aa522147c07ceaceef
Kunal Kaushik <kunkaush1@publicisgroupe.net>
2022-04-02 00:52:54 +0530

    Creating a middleware to decode user from a JWT

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,9 +3,12 @@ const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+var jwt = require('jsonwebtoken');
+var fetchuser = require('../middleware/fetchuser')
+
 const JWT_SECRET ='kunalaaryaharsh@123'
-//Create a user using: POST "/api/auth/createuser".No login required
+
+//ROUTE 1: Create a user using: POST "/api/auth/createuser".No Login Required
 router.post(
   "/createuser",
   [
@@ -27,7 +30,6 @@ router.post(
       }
       const salt = await bcrypt.genSalt(10)
       const secPass = await bcrypt.hash(req.body.password,salt)
-
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
@@ -46,20 +48,18 @@ router.post(
     }
   })
 
-//Authenticate a user using: POST "/api/auth/login".
+//ROUTE 2: Authenticate a user using: POST "/api/auth/login". No Login Required
 router.post("/login", [
     body("email", "Enter a valid email").isEmail(),
     body("password", "Password can't be blank").exists()
-  ], async (req, res) => {
-    
+  ],   
+  async (req, res) => {    
     //If there is an error  return bad request and error
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-
     const {email, password} = req.body
-
     try {
       let user = await User.findOne({email})
       if(!user){
@@ -69,7 +69,6 @@ router.post("/login", [
       if(!passwordCompare){
         return res.status(400).json({error:"Please Try to Login with Correct Credentials"})
       }
-
       const data = {
         user:{
           id:user.id
@@ -83,4 +82,17 @@ router.post("/login", [
     }
   })
 
+//ROUTE 3: Get LoggedIn User Details Create a user using: POST "/api/auth/getuser". Login Required
+router.post("/getuser", fetchuser, async (req, res) => {
+
+try {
+  userId = req.user.id
+  const user = await User.findById(userId).select("-password")
+  res.send(user)
+} catch (error) {
+  console.error(error.message)
+  res.status(500).send("Internal Server Error.")
+}
+
+})
 module.exports = router;
