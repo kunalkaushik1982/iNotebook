2c6d9b50d771eaff6ea51e937256160168a5c620
Kunal Kaushik <kunkaush1@publicisgroupe.net>
2022-04-01 23:39:18 +0530

    Creating Login Endpoint & sending auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,9 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET ='kunalaaryaharsh@123'
 //Create a user using: POST "/api/auth/createuser".No login required
 router.post(
   "/createuser",
@@ -23,12 +25,22 @@ router.post(
       if (user) {
         return res.status(400).json({ error: "User with this email already present." });
       }
+      const salt = await bcrypt.genSalt(10)
+      const secPass = await bcrypt.hash(req.body.password,salt)
+
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass
       });
-      res.json(user);
+      const data = {
+        user:{
+          id:user.id
+        }
+      }
+
+      const authtoken = jwt.sign(data,JWT_SECRET)
+      res.json({authtoken})
     } catch (error) {
         console.error(error.message)
         res.status(500).send("Some Error Occured.")
