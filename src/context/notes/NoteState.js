a2f36f7a1cd1a37fc0358189ace94f8285171e76
Kunal Kaushik <kunkaush1@publicisgroupe.net>
2022-04-02 19:40:52 +0530

    iNotebook: Adding NoteItem in a Separate Note component

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -13,6 +13,33 @@ const NoteSate = (props)=>{
           "__v": 0
         },
         {
+            "_id": "62475be87b55ece6311e4b53",
+            "user": "6247394004197452e39e134d",
+            "title": "My First note",
+            "description": "my first description",
+            "tag": "personal",
+            "timestamp": "2022-04-01T20:08:03.284Z",
+            "__v": 0
+        },
+        {
+            "_id": "62475be87b55ece6311e4b53",
+            "user": "6247394004197452e39e134d",
+            "title": "My First note",
+            "description": "my first description",
+            "tag": "personal",
+            "timestamp": "2022-04-01T20:08:03.284Z",
+            "__v": 0
+        },
+        {
+            "_id": "62475be87b55ece6311e4b53",
+            "user": "6247394004197452e39e134d",
+            "title": "My First note",
+            "description": "my first description",
+            "tag": "personal",
+            "timestamp": "2022-04-01T20:08:03.284Z",
+            "__v": 0
+        },
+        {
           "_id": "62475be97b55ece6311e4b55",
           "user": "6247394004197452e39e134d",
           "title": "My First note",
@@ -29,6 +56,15 @@ const NoteSate = (props)=>{
           "tag": "personal",
           "timestamp": "2022-04-01T20:08:03.284Z",
           "__v": 0
+        }, 
+        {
+            "_id": "62475be87b55ece6311e4b53",
+            "user": "6247394004197452e39e134d",
+            "title": "My First note",
+            "description": "my first description",
+            "tag": "personal",
+            "timestamp": "2022-04-01T20:08:03.284Z",
+            "__v": 0
         }
       ]
 
