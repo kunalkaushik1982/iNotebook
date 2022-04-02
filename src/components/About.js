2c1ebc16d9b5eaa57929b0b9d4bb982bfa9606ae
Kunal Kaushik <kunkaush1@publicisgroupe.net>
2022-04-02 19:03:07 +0530

    useLocation Hook in React

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,9 @@
 import React, { useContext,useEffect } from "react";
-import noteContext from "../context/notes/noteContext";
 
 const About = () => {
-  const a = useContext(noteContext)
-  useEffect(() => {
-    a.update()
-    //eslint-disable-next-line
-  }, []);
-  
-  return (
+   return (
   <div>
-    This is About {a.state.name} and he is in {a.state.class}
+    This is About Page
   </div>
   )
 };
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,22 +2,9 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteSate = (props)=>{
-    const s1 = {
-        "name": "Kunal",
-        "class":"X"
-    }
-    const [state,setState] =useState(s1)
-    const update = () =>{
-        setTimeout(() => {
-            setState({
-                "name": "Harsh",
-                "class":"IV"
-            })
-        }, 1000);
 
-    }
     return(
-        <NoteContext.Provider value={{state,update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
